a3cabf5169802f1ccf4d913e5c547520208f9b28
dzassist <dzassistdemo@gmail.com>
2025-06-21 00:55:26 +0100

    Update index.js Ajout route GET /

--- index.js
+++ index.js
@@ -28,3 +28,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Proxy listening on port ${PORT}`);
 });
+app.get('/', (req, res) => {
+  res.send('DzAssist Proxy fonctionne ✅');
+});
